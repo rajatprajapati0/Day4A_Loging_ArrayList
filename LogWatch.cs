0a4e43051d535ba7f91ef8dc0dd4fced41f11f1a
Rajat <rajatprajapati09@gmail.com>
2023-04-17 06:40:28 +0530

    [Rajat]Logging for calculator

---
+++ LogWatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace DemoLog
+{
+    internal class LogWatch
+    {
+        public static  readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        public void logInfo(string message)
+        {
+            logger.Info(message);
+        
+        }
+        public void logWarn(string message)
+        {
+           logger.Warn(message);
+        }               
+        public void logDebug(string message)
+        {
+           logger.Debug(message);
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLog;
+using System;
 
 namespace Logging
 {
@@ -6,7 +7,59 @@ namespace Logging
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LogWatch logWatch = new LogWatch();
+            logWatch.logDebug("************Start***********");
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine("calculator");
+            Console.WriteLine("Enter option \n'+'>Addition\n'-'Subtraction>\n'*'>multiplicaation\n'/'>division");
+            string B = Console.ReadLine();
+            char option = B[0];
+
+            Console.WriteLine("enter first value");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter second value");
+            int b = int.Parse(Console.ReadLine());
+
+
+            switch (option)
+            {
+
+                case '+':
+                    logWatch.logInfo("addition ");
+
+                    Console.WriteLine(calculator.add(a, b));
+                    logWatch.logDebug("opretion done");
+                    break;
+                case '-':
+                    logWatch.logInfo("Subtraction");
+
+                    Console.WriteLine(calculator.sub(a, b));
+                    logWatch.logDebug("opretion done");
+
+                    break;
+                case '*':
+                    logWatch.logInfo("multiplicaation");
+
+                    Console.WriteLine(calculator.mult(a, b));
+                    logWatch.logDebug("opretion done");
+
+                    break;
+                case '/':
+                    logWatch.logInfo("division");
+                    Console.WriteLine(calculator.div(a, b));
+                    logWatch.logDebug("opretion done");
+
+                    break;
+                default:
+                    logWatch.logWarn("wrong option");
+                    Console.WriteLine("Enter right option");
+
+                    break;
+
+            }
+            logWatch.logDebug("************Done***********\n");
+
         }
     }
 }
